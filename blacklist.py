338950f1203be089709ed188e7662abb47d7273b
LTA <37591675+TheCodingNapkin@users.noreply.github.com>
2020-08-09 02:04:39 -0500

    Initial Commit

---
+++ blacklist.py
@@ -0,0 +1,45 @@
+blacklist = [
+	"bedrock","potato","poisonous_potato","lava","water","air",
+	"farmland","spawner","tipped_arrow","bow","trident","crossbow",
+	"elytra","shulker_box","shield","dead_bush","end_portal_frame",
+	"turtle_egg","flint_and_steel","saddle","dragon_egg",
+	"fishing_rod","shears","command_block","zombie_spawn_egg",
+	"villager_spawn_egg","spectral_arrow"
+]
+blacklist_fuzzy = [
+	"spawn_egg","netherite","music_disc","potion","_helmet",
+	"_chestplate","_sword","_pickaxe","_leggings","_hoe","_axe",
+	"_horse_armor","head","map","shovel","boots","_book","bed",
+	"stew","soup","pattern","_bucket","_minecart","_a_stick",
+	"_anvil","gold","boat"
+]
+
+item_counts32 = [
+	"emerald"
+]
+
+item_counts16_fuzzy = [
+	"egg","ender_pearl","snowball","bucket","honey_bottle",
+	"_banners","_sign","beacon","nether_star","end_crystal",
+	"armor_stand","redstone_block","iron_block","ender_chest",
+	"_ore","name_tag","phantom_membrane"
+]
+item_counts8_fuzzy = [
+	"conduit","lapis_block","emerald_block","diamond_block",
+	"sea_pickle"
+]
+item_counts3_fuzzy = [
+	"totem_of_undying","experience_bottle","emerald_block"
+]
+
+if __name__ == "__main__":
+	print(
+		"""
+		Blacklists: \"{}\", \"{}\"
+		Blacklists (fuzzy): \"{}\", \"{}\", \"{}\", \"{}\"
+		""".replace("\t","").format(
+			blacklist,item_counts32,
+			blacklist_fuzzy,item_counts16_fuzzy,
+			item_counts8_fuzzy,item_counts3_fuzzy
+		)
+	)
---
+++ file_creator.py
@@ -0,0 +1,118 @@
+"""
+creates files with specific names
+CREDIT: Rico Alexander, cheelo21
+"""
+import os, blacklist
+from string import digits
+
+item_counts = {
+	32:[],
+	16:[],
+	8:[],
+	3:[]
+}
+
+#################################################
+
+f = open("Minecraft ID List.txt", "r")
+data = f.read().replace("\t", "").replace(" ", "")
+f.close()
+for digit in digits:
+	data = data.replace(str(digit),"")
+data = data.split("\n")
+try: os.system("cls")
+except OSError: pass
+
+for c, line in enumerate(data):
+	if not line[:11] == "(minecraft:" and line[::-1][:1] == ")":
+		data.pop(c)
+#running the blacklist checks in a loop was contributed by cheelo21
+for i in range(100):
+	for c, line in enumerate(data):
+		for item in blacklist.blacklist:
+			if item == line.strip(")")[11:]:
+				print("Banned: \"{0}\"".format(data[c].strip(")")[11:]),end=" ")
+				print("from: \"blacklist\" and kwarg: \"{}\"".format(item))
+				data.pop(c)
+				break
+#running the blacklist checks in a loop was contributed by cheelo21
+for i in range(100):
+	for c, line in enumerate(data):
+		for item in blacklist.blacklist_fuzzy:
+			if item in line.strip(")")[11:]:
+				print("Banned: \"{0}\"".format(data[c].strip(")")[11:]),end=" ")
+				print("from: \"blacklist_fuzzy\" and kwarg: \"{}\"".format(item))
+				data.pop(c)
+				break
+print("\n"+("#"*50+"\n")*3)
+
+################################################
+
+for c, line in enumerate(data):
+	for item in blacklist.item_counts32:
+		if "minecraft:"+item == line[1:][::-1][1:][::-1]:
+			print("Changed: \"{0}\"".format(data[c].strip(")")[11:]),end=" ")
+			print("to: \"32\" from kwarg: \"{}\"".format(item))
+			item_counts[32].append(item)
+	for item in blacklist.item_counts16_fuzzy:
+		if item in line.strip(")")[11:]:
+			print("Changed: \"{0}\"".format(data[c].strip(")")[11:]),end=" ")
+			print("to: \"16\" from kwarg: \"{}\"".format(item))
+			item_counts[16].append(item)
+	for item in blacklist.item_counts8_fuzzy:
+		if item in line.strip(")")[11:]:
+			print("Changed: \"{0}\"".format(data[c].strip(")")[11:]),end=" ")
+			print("to: \"8\" from kwarg: \"{}\"".format(item))
+			item_counts[8].append(item)
+	for item in blacklist.item_counts3_fuzzy:
+		if item in line.strip(")")[11:]:
+			print("Changed: \"{0}\"".format(data[c].strip(")")[11:]),end=" ")
+			print("to: \"3\" from kwarg: \"{}\"".format(item))
+			item_counts[3].append(item)
+print("\n"+("#"*50+"\n")*3)
+
+#################################################
+
+os.chdir("dump")
+filenames = []
+for c, line in enumerate(data):
+	if line[:11] == "(minecraft:" and line[::-1][:1] == ")":
+		line = line.strip("(").strip(")")
+		if line in filenames:
+			continue
+		filenames.append(line)
+
+#################################################
+
+f = open("../template.json", "r")
+template = f.read().split("\n")
+f.close()
+
+for filename in filenames:
+	data = []
+	for line in template:
+		if "|INJECT|" in line:
+			line = line.replace("|INJECT|", filename)
+		if "|COUNT INJECT|" in line:
+			for item_count in item_counts[32]:
+				if item_count in filename:
+					line = line.replace("|COUNT INJECT|", "32")
+			for item_count in item_counts[16]:
+				if item_count in filename:
+					line = line.replace("|COUNT INJECT|", "16")
+			for item_count in item_counts[8]:
+				if item_count in filename:
+					line = line.replace("|COUNT INJECT|", "8")
+			for item_count in item_counts[3]:
+				if item_count in filename:
+					line = line.replace("|COUNT INJECT|", "3")
+
+		if "|COUNT INJECT|" in line:
+			line = line.replace("|COUNT INJECT|", "64")
+		data.append(line)
+
+	filename = filename.replace("minecraft:", "")+".json"
+	f = open(filename, "w")
+	f.write("\n".join(data))
+	f.close()
+	print("Created: \"{}\"".format(filename))
